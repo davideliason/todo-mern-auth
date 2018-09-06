341868807caac63c4b0e547f739b7b2daa7bb1ff
David Eliason <davethemaker@gmail.com>
2018-09-06 11:29:23 -0700

    spun up server with single route and with local passport strategy implemented

--- app.js
+++ app.js
@@ -5,4 +5,36 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const passport = require('passport');
 const session = require('express-session');
-const localStrategy = require('passport-local');
+const LocalStrategy = require('passport-local').Strategy;
+
+
+const app = express();
+
+// passport config
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+      User.findOne({ username: username }, function(err, user) {
+        if (err) { return done(err); }
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username.' });
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+        return done(null, user);
+      });
+    }
+  ));
+
+// middleware
+app.use(express.static("public"));
+app.use(session({ secret: "coffee" }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/', (req,res) => {
+    res.send('todos on the way');
+});
+
+app.listen(3000);
