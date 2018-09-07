a17ae47324e44db5071c5a6c08b943d464909402
David Eliason <davethemaker@gmail.com>
2018-09-07 11:43:48 -0700

    move login form to own file

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 // run packages
 const express = require('express');
-const path = require('path');
+const multer = require('multer');
 const bodyParser = require('body-parser');
+
+const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const passport = require('passport');
 const session = require('express-session');
 const LocalStrategy = require('passport-local').Strategy;
-const multer = require('multer');
 
 // setup
 const app = express();
@@ -36,13 +37,52 @@ app.use('/', express.static(path.join(__dirname,'public')));
 // app.use(passport.initialize());
 // app.use(passport.session());
 
+const multerConfig = {
+
+    //specify diskStorage (another option is memory)
+    storage: multer.diskStorage({
+
+      //specify destination
+      destination: function(req, file, next){
+        next(null, './public/photo-storage');
+      },
+
+      //specify the filename to be unique
+      filename: function(req, file, next){
+        console.log(file);
+        //get the file mimetype ie 'image/jpeg' split and prefer the second value ie'jpeg'
+        const ext = file.mimetype.split('/')[1];
+        //set the file fieldname to a unique name containing the original name, current datetime and the extension.
+        next(null, file.fieldname + '-' + Date.now() + '.'+ext);
+      }
+    }),
+
+    // filter out and prevent non-image files.
+    fileFilter: function(req, file, next){
+          if(!file){
+            next();
+          }
+
+        // only permit image mimetypes
+        const image = file.mimetype.startsWith('image/');
+        if(image){
+          console.log('photo uploaded');
+          next(null, true);
+        }else{
+          console.log("file not supported")
+          //TODO:  A better message response to user on failure.
+          return next();
+        }
+    }
+  };
+
 // routes
 app.get('/', (req, res) => {
     res.render('index.html');
 });
-app.get('/welcome', (req,res) => {
-    res.send('welcome');
-});
+// app.get('/welcome', (req,res) => {
+//     res.send('welcome');
+// });
 
 // app.post('/login',
 //   passport.authenticate('local', { successRedirect: '/welcome',
@@ -50,4 +90,6 @@ app.get('/welcome', (req,res) => {
 //                                    failureFlash: true })
 // );
 
-app.listen(port);
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
